9f13d732d05bc70bc5d780faeee8e5bb6c4a4caa
Siddhesh Ashtekar <86523900+ashtekarsiddhesh25@users.noreply.github.com>
2025-09-17 00:52:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 # Convert the snowpark dataframe to padas dataframe
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 
 
